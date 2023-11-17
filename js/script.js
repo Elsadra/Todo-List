7238008e684ae6c08df4110183ca022489c192cb
SADRA <134719329+Elsadra@users.noreply.github.com>
2023-11-17 19:48:09 +0330

    ROSE Todo-List
    Test

---
+++ js/script.js
@@ -0,0 +1,35 @@
+﻿let $ = document;
+let todos = $.querySelector(".todos");
+let deleteBtn = $.querySelectorAll(".delete");
+let input = $.querySelector("input");
+
+input.addEventListener("keydown", function (event) {
+  let inputValue = input.value.trim();
+  if (event.key == "Enter") {
+    event.preventDefault();
+    if (input.value == "") {
+      alert("Please write the unfinished work");
+    } else {
+      let liNode = $.createElement("li");
+      let iNode = $.createElement("i");
+      liNode.className =
+        "list-group-item d-flex justify-content-between align-items-center";
+      iNode.setAttribute("class", "fa fa-trash-o delete");
+      liNode.innerHTML += `
+          <span>${inputValue}</span>
+            `;
+      liNode.append(iNode);
+      todos.append(liNode);
+      iNode.addEventListener("click", function (eventINode) {
+        eventINode.target.parentElement.remove();
+      });
+      input.value = "";
+    }
+  }
+});
+
+deleteBtn.forEach(function (event) {
+  event.addEventListener("click", function () {
+    event.parentElement.remove();
+  });
+});
